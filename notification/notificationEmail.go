b69e20e29de2b8452930fb4018d99935b2a12119
alanfg120 <aherrera@asesoftware.com>
2024-10-26 17:25:33 +0000

    sengrid integration

--- notification/notificationEmail.go
+++ notification/notificationEmail.go
@@ -2,9 +2,26 @@ package notification
 
 import (
 	"log"
+	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-
 func (n *Notification) SendEmailNotification() {
-	log.Println("Sending email notification" + n.GetNotificationMessage())
+	from := mail.NewEmail("Example User", "test@example.com")
+	subject := ""
+	to := mail.NewEmail("Correo estudiante", n.student.Correo)
+	plainTextContent := n.GetNotificationMessage()
+	htmlContent := ""
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println(response.StatusCode)
+		log.Println(response.Body)
+		log.Println(response.Headers)
+	}
 }
